fabfdffcbede1ac04eb71455ab25d885c3ab4a42
dbarandica <142930222+dbarandica@users.noreply.github.com>
2024-03-05 09:09:25 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
 # This file was created by: Daniel Barandica
-
-# import libraries and modules
+# import libraries
 import pygame as pg
 from settings import *
 from sprites import *
 from random import randint
 import sys
 from os import path
-
-
-# Define game class...
+    
+#makes Game a class
 class Game:
-    # Define a special method to init the properties of said class...
+    # initializes the game
     def __init__(self):
-        # init pygame
+        #initializes the game (pygame)
         pg.init()
-        # set size of screen and be the screen
+        # sets the window (width, height and title)
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
         pg.display.set_caption(TITLE)
-        # setting game clock 
+        # turns on time
         self.clock = pg.time.Clock()
         self.load_data()
     def load_data(self):
         game_folder = path.dirname(__file__)
         self.map_data = []
+        # 'r'     open for reading (default)
+        # 'w'     open for writing, truncating the file first
+        # 'x'     open for exclusive creation, failing if the file already exists
+        # 'a'     open for writing, appending to the end of the file if it exists
+        # 'b'     binary mode
+        # 't'     text mode (default)
+        # '+'     open a disk file for updating (reading and writing)
+        # 'U'     universal newlines mode (deprecated)
+        # below opens file for reading in text mode
+        # with 
         '''
         The with statement is a context manager in Python. 
         It is used to ensure that a resource is properly closed or released 
@@ -33,15 +41,13 @@ class Game:
             for line in f:
                 print(line)
                 self.map_data.append(line)
-
-    # Create run method which runs the whole GAME
+    # runs the game
     def new(self):
-        print("create new game...")
         self.all_sprites = pg.sprite.Group()
         self.walls = pg.sprite.Group()
-        # self.player1 = Player(self, 1, 1)
-        # for x in range(10, 20):
-        #     Wall(self, x, 5)
+        self.coins = pg.sprite.Group()
+        self.bushes = pg.sprite.Group()
+        self.power_ups = pg.sprite.Group()
         for row, tiles in enumerate(self.map_data):
             print(row)
             for col, tile in enumerate(tiles):
@@ -50,28 +56,34 @@ class Game:
                     print("a wall at", row, col)
                     Wall(self, col, row)
                 if tile == 'P':
-                    self.player = Player(self, col, row)
+                    self.player1 = Player(self, col, row)
+                if tile == 'C':
+                    Coin(self,col,row)
+                if tile == 'B':
+                    Bush(self,col,row)
+                if tile == 'U':
+                    PowerUp(self,col,row)
 
     def run(self):
-        # 
+        # creates "while" loop that triggers when running = true
         self.playing = True
         while self.playing:
-            self.dt = self.clock.tick(FPS) / 1000
+            self.dt = self.clock.tick(FPS)/1000
             self.events()
             self.update()
             self.draw()
     def quit(self):
-         pg.quit()
-         sys.exit()
+        pg.quit()
+        sys.exit()
 
     def update(self):
         self.all_sprites.update()
-    
+
     def draw_grid(self):
-         for x in range(0, WIDTH, TILESIZE):
-              pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
-         for y in range(0, HEIGHT, TILESIZE):
-              pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
+        for x in range(0, WIDTH, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (x,0) , (x,HEIGHT))
+        for y in range(0, WIDTH, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (0,y) , (WIDTH, y))
 
     def draw(self):
             self.screen.fill(BGCOLOR)
@@ -80,24 +92,47 @@ class Game:
             pg.display.flip()
 
     def events(self):
-         for event in pg.event.get():
+        for event in pg.event.get():
             if event.type == pg.QUIT:
                 self.quit()
             if event.type == pg.KEYDOWN:
-                if event.key == pg.K_LEFT:
-                    self.player.move(dx=-1)
-                if event.key == pg.K_RIGHT:
-                    self.player.move(dx=1)
-                if event.key == pg.K_UP:
-                    self.player.move(dy=-1)
-                if event.key == pg.K_DOWN:
-                    self.player.move(dy=1)
+                if event.key == pg.K_r:
+                    self.player1.image.fill(RED)
+                if event.key == pg.K_g:
+                    self.player1.image.fill(GREEN)
+                if event.key == pg.K_b:
+                    self.player1.image.fill(BLUE)
+            # if event.type == pg.KEYDOWN:
+            #     if event.key == pg.K_LEFT:
+            #         self.player1.move(dx=-1)
+            #     if event.key == pg.K_RIGHT:
+            #         self.player1.move(dx=1)
+            #     if event.key == pg.K_UP:
+            #         self.player1.move(dy=-1)
+            #     if event.key == pg.K_DOWN:
+            #         self.player1.move(dy=1)
+            #     if event.key == pg.K_a:
+            #         self.player1.move(dx=-1)
+            #     if event.key == pg.K_d:
+            #         self.player1.move(dx=1)
+            #     if event.key == pg.K_w:
+            #         self.player1.move(dy=-1)
+            #     if event.key == pg.K_s:
+            #         self.player1.move(dy=1)
+            #     if event.key == pg.K_SPACE:
+            #         self.player1.image.fill == (RED)
+            #         pass
+                    
+            
 
-# Instantiate the game... 
+
+    
+
+# Instantiates the game...
 g = Game()
 # use game method run to run
-# g.show_start_screen()
 while True:
     g.new()
     g.run()
-    # g.show_go_screen()
+    #g.show_start_screen()
+ 
