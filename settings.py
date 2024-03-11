496aa6641f7cd1c181fa9016feee0313e5a42498
dbarandica <142930222+dbarandica@users.noreply.github.com>
2024-03-11 12:45:51 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,18 +1,27 @@
+
+# define values for width and height
 WIDTH = 1024
 HEIGHT = 768
 
-FPS = 30
+# set frames per second
+FPS = 60
 
-TITLE = "My Game"
+# define the title of the game
+TITLE = "My Fun Game"
 
+# define the size of the game's title
 TILESIZE = 32
-WHITE = (255,255,255)
-GREEN =  (0,255,0)
-RED = (255,0,0)
-LIGHTGREY = (75,75,75)
-BLUE = (0,0,255)
-YELLOW = (255,255,0)
-BGCOLOR= (0,0,0)
-PURPLE = (255,0,255)
 
+# define RGB values for color variables used in the game
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+LIGHTGREY = (150, 150, 150)
+BGCOLOR = (0, 0, 0)
+YELLOW = (255, 255, 0)
+ORANGE = (255, 165, 0)
+
+POWER_UP_EFFECTS = ["Speed", "Camo"]
+
+# Player settings
 PLAYER_SPEED = 300
